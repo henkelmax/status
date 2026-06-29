7945c97915a928b56462dad1f3b88a73a23f3cec
HughBone <hughbone27@gmail.com>
2026-06-29 19:02:08 -0400

    add announce status on join functionality

--- src/main/java/de/maxhenkel/status/playerstate/Availability.java
+++ src/main/java/de/maxhenkel/status/playerstate/Availability.java
@@ -1,17 +1,33 @@
 package de.maxhenkel.status.playerstate;
 
+import net.minecraft.ChatFormatting;
+
 public enum Availability {
 
-    NONE(""), DO_NOT_DISTURB("do_not_disturb"), OPEN("open");
+    NONE("", "message.status.no_availability", ChatFormatting.WHITE),
+    DO_NOT_DISTURB("do_not_disturb", "message.status.do_not_disturb", ChatFormatting.RED),
+    OPEN("open", "message.status.open", ChatFormatting.GREEN);
 
     private final String name;
+    private final String translationKey;
+    private final ChatFormatting color;
 
-    Availability(String name) {
+    Availability(String name, String translationKey, ChatFormatting color) {
         this.name = name;
+        this.translationKey = translationKey;
+        this.color = color;
     }
 
     public String getName() {
         return name;
+    }
+
+    public String getTranslationKey() {
+        return translationKey;
+    }
+
+    public ChatFormatting getColor() {
+        return color;
     }
 
     public static Availability byName(String name) {
