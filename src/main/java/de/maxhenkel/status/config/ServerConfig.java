abafdb8f5b58763b716f4a3e074b0a7202e2b0b9
HughBone <hughbone27@gmail.com>
2026-06-29 19:00:30 -0400

    add announceStatusesOnLogin serverconfig option

--- src/main/java/de/maxhenkel/status/config/ServerConfig.java
+++ src/main/java/de/maxhenkel/status/config/ServerConfig.java
@@ -8,11 +8,13 @@ public class ServerConfig {
     public final ConfigEntry<String> noSleepTitle;
     public final ConfigEntry<String> noSleepPlayerSubtitle;
     public final ConfigEntry<String> noSleepMultipleSubtitle;
+    public final ConfigEntry<Boolean> announceStatusesOnLogin;
 
     public ServerConfig(ConfigBuilder builder) {
         noSleepTitle = builder.stringEntry("no_sleep_title", "No Sleep");
         noSleepPlayerSubtitle = builder.stringEntry("no_sleep_player_subtitle", "%s does not want you to sleep");
         noSleepMultipleSubtitle = builder.stringEntry("no_sleep_multiple_subtitle", "Some players do not want you to sleep");
+        announceStatusesOnLogin = builder.booleanEntry("announce_statuses_on_login", false);
     }
 
 }
